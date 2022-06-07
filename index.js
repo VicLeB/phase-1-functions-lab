051d26c6950bf6a6ffa79f658f5d8d2f724665de
Victoria LeBel <vickileb@hotmail.com>
2022-06-06 21:58:50 -0700

    commit tree 37cbe16dfc6d468c511e981ee3534a4cf0645459

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    return Math.abs(blocks - 42)
+};
+
+distanceFromHqInBlocks(43);
+
+function distanceFromHqInFeet(blocks){
+    return distanceFromHqInBlocks(blocks) * 264
+};
+
+distanceFromHqInFeet();
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start-destination) *264
+};
+
+distanceTravelledInFeet();
+
+function calculatesFarePrice(start, destination){
+    let distance = Math.abs(start- destination) * 264
+    if (distance <= 400){
+        return 0
+    } else if (distance >400 && distance <2000){
+        let price = (distance * 0.02 - 8).toFixed(2);
+        return parseFloat(price);
+    } else if (distance >= 2000 && distance <= 2500){
+        return 25 
+    }else if (distance >2500){return "cannot travel that far"
+    }
+}
